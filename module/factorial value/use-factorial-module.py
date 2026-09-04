7c36028d272e45a0edd6ece8677b77aff368bfe6
safin <mahfuzsafin@gmail.com>
2026-09-04 15:19:40 +0600

    factorial value use module

---
+++ module/factorial value/use-factorial-module.py
@@ -0,0 +1,6 @@
+import factorial_module
+num = int(input("Enter a Number: "))
+
+result = factorial_module.factorial(num)
+
+print("Factorial Value Is = " , result)
---
+++ module/use-module.py
@@ -0,0 +1,5 @@
+import createModule
+print(createModule.add(5, 10))
+print(createModule.div(20, 5))
+print(createModule.sub(20, 5))
+print(createModule.mul(10, 2))
