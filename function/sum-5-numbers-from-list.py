5ac2ef42c99a86fde1b4dfee708458cae0d31b1f
safin <mahfuzsafin@gmail.com>
2026-09-04 11:38:18 +0600

    sum of 5 list numbers

--- function/sum-5-numbers-from-list.py
+++ function/sum-5-numbers-from-list.py
@@ -1,6 +1,6 @@
 # sum of 5 numbers from a list
 
-numbers = input("Enter the 5 numbers: ").split()
+numbers = input("Enter the numbers: ").replace("," , " ").split()
 numbers = [int(i) for i in numbers]
 def sum_list(numbers):
 
@@ -12,3 +12,20 @@ def sum_list(numbers):
 
 result = sum_list(numbers)
 print(result)
+
+
+
+# -------------------------------------------------------------------
+# 
+
+def SUM_LIST(NUMBERS):
+
+  TOTAL = 0
+  for i in NUMBERS:
+    TOTAL += i
+
+  return TOTAL
+
+
+RESULT = SUM_LIST([25,45,74,85,95])
+print(RESULT)
